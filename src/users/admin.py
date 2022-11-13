fcd010999475120092f6d3a9b32b1269cf30dffe
unknown <ehsansarikhani8888@gmail.com>
2022-11-13 10:23:45 +0330

    Added custom user model

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ["email", "first_name", "last_name", "last_login", "is_active", "is_staff", "is_superuser"]
+    list_display_links = ["email", "first_name", "last_name"]
+    list_editable = ["is_active", "is_staff"]
+    readonly_fields = ["password", "last_login", "created", "last_update"]
+    ordering = ["-created", "-pk"]
+    filter_horizontal = []
+    list_filter = []
+    fieldsets = [
+        [None, {"fields": ["email", "password"]}],
+        ["Personal Info", {"fields": ["first_name", "last_name"]}],
+        ["Permissions", {"fields": ["is_active", "is_staff", "is_superuser"]}],
+        ["Important Dates", {"fields": ["created", "last_update", "last_login"]}],
+    ]
+    add_fieldsets = [
+        [None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2")
+        }],
+    ]
+
+
+admin.site.register(User, CustomUserAdmin)
--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, *args, **kwargs):
+        if not email:
+            raise ValueError("Account must have Email Address.")
+        user = self.model(email=self.normalize_email(email))
+        user.save(using=self._db)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, *args, **kwargs):
+        user = self.create_user(email, password, *args, **kwargs)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_staff
+
+    def has_module_perms(self, add_label):
+        return True
